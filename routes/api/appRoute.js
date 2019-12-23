3de223ef01b93b6a2dfe156df4e9460410164632
neerajp99 <neerajp1999@gmail.com>
2019-12-24 04:37:03 +0530

    Add methods for downloading a file using filename

--- routes/api/appRoute.js
+++ routes/api/appRoute.js
@@ -7,7 +7,7 @@ const multer = require("multer");
 const path = require("path");
 
 //Multer storage directory
-const storageDirectory = path.join(__dirname, "..", "storage");
+const storageDirectory = path.join(__dirname, "..", "uploadFiles");
 
 // Multer - File Storage configuration
 // The disk storage engine gives you full control on storing files to disk.
@@ -24,11 +24,11 @@ const storage = multer.diskStorage({
   }
 });
 // add the destination to multer
-const upload = multer({ storageDirectory: storageDirectory });
+const upload = multer({ storages: storage });
 
 // @route GET /api/appRoute/
 // @description Get
-// @acess Public
+// @access Public
 router.get("/", (req, res) => {
   // console.log(req.app.get('upload'))
   return res.status(200).json({
@@ -38,12 +38,28 @@ router.get("/", (req, res) => {
 
 // @route POST /api/appRoute/
 // @description Post
-// @acess Public
-router.post("/api/upload", upload.array("files"), function(req, res, next) {
+// @access Public
+router.post("/upload", upload.array("files"), (req, res, next) => {
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
   console.log("Received files", req.files);
-  return res.status(200).json("It works");
+  return res.status(200).json({
+    files: req.files
+  });
+});
+
+// @route GET /api/appRoute/
+// @description Transfering/Downloading the file at path as an “attachment”
+// @access Public
+router.get("/download/:filename", (req, res) => {
+  const filePath = path.join(storageDirectory, req.params.filename);
+  return res.download(filePath, req.params.filename, error => {
+    if (error) {
+      return res.status(400).json(error);
+    } else {
+      console.log("File is downloaded!");
+    }
+  });
 });
 
 module.exports = router;
