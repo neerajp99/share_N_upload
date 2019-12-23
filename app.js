6e2fe028b47b5ee445c56e053d2cbd1c09fee694
neerajp99 <neerajp1999@gmail.com>
2019-12-23 16:58:00 +0530

    Add multer configuration for file upload

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const keys = require("./config/keys");
+// import multer for storage
+const multer = require("multer");
 
 // Initialise express app
 const app = express();
@@ -13,6 +16,26 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+//Multer storage directory
+const directory = path.join(__dirname, "..", "storage");
+
+// Multer - File Storage configuration
+// The disk storage engine gives you full control on storing files to disk.
+// For info https://github.com/expressjs/multer#diskstorage
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, directory);
+  },
+  filename: (req, file, cb) => {
+    // returns the extension of a file path.
+    // for more info: https://github.com/expressjs/multer#api
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+// add the destination to multer
+const upload = multer({ storage: destination });
 
 // Add database connection
 const db = keys.mongoURI;
