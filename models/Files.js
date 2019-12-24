d5293170401f45b6c96120c11f4fa27978ae838b
neerajp99 <neerajp1999@gmail.com>
2019-12-24 21:01:56 +0530

    Update model

--- models/Files.js
+++ models/Files.js
@@ -3,28 +3,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-  file: {
-    fileName: {
-      type: String,
-      required: true
-    },
-    originalName: {
-      type: String,
-      required: true
-    },
-    mimetype: {
-      type: String,
-      required: true
-    },
-    size: {
-      type: Integer,
-      required: true
-    },
-    created: {
-      type: Date,
-      default: Date.now()
-    }
+  fileName: {
+    type: String,
+    required: true
+  },
+  originalName: {
+    type: String,
+    required: true
+  },
+  mimetype: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now()
   }
 });
 
-module.exports = Files = mongoose.models("files", fileSchema);
+module.exports = Files = mongoose.model("files", fileSchema);
