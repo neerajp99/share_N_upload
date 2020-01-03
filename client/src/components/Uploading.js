562a86a4124700a8ef6e5881b32777656594941a
neerajp99 <neerajp1999@gmail.com>
2020-01-03 22:14:03 +0530

    Initial page for uploading status

---
+++ client/src/components/Uploading.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Landing from "./Landing";
+
+class Uploading extends Component {
+  render() {
+    console.log("Uploaded stuff");
+    return (
+      <div className="uploading_form">
+        <h4 className="">Lalalaala, uploading will take place here</h4>
+      </div>
+    );
+  }
+}
+
+export default Uploading;
