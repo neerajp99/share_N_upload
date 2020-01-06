ff8889f998de268b6570436ff8c0a708b396d97d
neerajp99 <neerajp1999@gmail.com>
2020-01-07 03:26:59 +0530

    Add routings

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Landing from './components/Landing'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+import SharedContent from "./components/shared/SharedContent";
 
 function App() {
   return (
-
-  <Router>  <div className="App">
-      <Route exact path="/" render={() => <Landing />}/>
-    </div></Router>
+    <Router>
+      {" "}
+      <div className="App">
+        <Route exact path="/" render={() => <Landing />} />
+        <Route exact path="/shared/:id" component={SharedContent} />
+      </div>
+    </Router>
   );
 }
 
