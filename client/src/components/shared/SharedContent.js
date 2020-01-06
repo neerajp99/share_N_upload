be6c8c4f3f410def1c1d1d263996dadb4eae67a9
neerajp99 <neerajp1999@gmail.com>
2020-01-07 03:28:35 +0530

    Intial method to view shared content

---
+++ client/src/components/shared/SharedContent.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class SharedContent extends Component {
+
+  componentDidMount () {
+    // console.log(this.props.match)
+  }
+  render () {
+    return (
+      <div className="sharedContent">
+        <h4 className="">Ollalalalal</h4>
+      </div>
+    )
+  }
+}
+
+export default SharedContent
