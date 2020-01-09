fe5e8911b63c6f013eefc6ebf9a1703971639706
neerajp99 <neerajp1999@gmail.com>
2020-01-10 05:28:38 +0530

    Remove extra validation

--- validation/is-empty.js
+++
@@ -1,8 +0,0 @@
-// function that checks for empty, undefined etc strings
-const isEmpty = value =>
-  value === undefined ||
-  value === null ||
-  (typeof value === "object" && Object.keys(value).length === 0) ||
-  (typeof value === "string" && value.trim().length === 0);
-
-module.exports = isEmpty;
