7850e1b55123aff4b0a29f9326f1112fe73f6aea
neerajp99 <neerajp1999@gmail.com>
2020-01-11 02:37:18 +0530

    Add Provider to the app;

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,44 @@ import Landing from "./components/Landing";
 import SharedContent from "./components/shared/SharedContent";
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
+import { Provider } from "react-redux";
+import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
+// Check if the user is already present in localstorage
+// if yes, then authenticate the user
+if (localStorage.authToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.authToken);
+  //Decode the tokens ang get user infe ormation
+  const decodedToken = jwt_decode(localStorage.authToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decodedToken));
+  //Check for expired tokens
+  const currentTime = Date.now() / 1000;
+  if (decodedToken.exp < currentTime) {
+    // log out user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.alert("Session expired, please login again.");
+    window.location.href = "/login";
+  }
+}
 function App() {
   return (
-    <Router>
-      {" "}
-      <div className="App">
-        <Route exact path="/" render={() => <Landing />} />
-        <Route exact path="/api/shared/:id" component={SharedContent} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        {" "}
+        <div className="App">
+          <Route exact path="/" render={() => <Landing />} />
+          <Route exact path="/api/shared/:id" component={SharedContent} />
+          <Route exact path="/register" render={() => <Register />} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
