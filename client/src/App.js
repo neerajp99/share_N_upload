88ddfe57978602eb02142a6828ca8d80c3faf7a4
neerajp99 <neerajp1999@gmail.com>
2020-01-10 21:46:34 +0530

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import SharedContent from "./components/shared/SharedContent";
+import Register from "./components/authentication/Register";
+import Login from "./components/authentication/Login";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <div className="App">
         <Route exact path="/" render={() => <Landing />} />
         <Route exact path="/api/shared/:id" component={SharedContent} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
     </Router>
   );
