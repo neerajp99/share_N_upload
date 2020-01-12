92c94548870b764202a85b0a4ebc9a5ce6c677a2
neerajp99 <neerajp1999@gmail.com>
2020-01-12 13:20:06 +0530

    Add files details model

---
+++ models/FilesDetails.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const FilesDetailsSchema = new Schema({
+  from: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  files: []
+});
+
+module.exports = FilesDetails = mongoose.model(
+  "files-details",
+  FilesDetailsSchema
+);
